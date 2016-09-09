1c83be9c5460c5294f80eecf9244a73d935fc29c
TonyRoberson <tonyjroberson@gmail.com>
2016-09-08 21:48:12 -0500

    Add load package function call

--- Data/loadDataScript.R
+++ Data/loadDataScript.R
@@ -1,14 +1,17 @@
-# Load first split half for EFA
-sample1 <- read.spss(file = "HBSC Bullying Scale_S1_EFA.sav", 
+# Load import package
+library(foreign)
+
+# Import first split half for EFA
+s1 <- read.spss(file = "HBSC Bullying Scale_S1_EFA.sav", 
                      use.value.labels = FALSE, 
                      to.data.frame = TRUE)
 
-# Load second split half for CFA
-sample2 <- read.spss(file = "HBSC Bullying Scale_S2_CFA.sav", 
+# Import second split half for CFA
+s2 <- read.spss(file = "HBSC Bullying Scale_S2_CFA.sav", 
           use.value.labels = FALSE,
           to.data.frame = TRUE)
 
-# Load only relevant variables for CFA
-sample2rel <- read.spss(file = "S2_relevant_variables.sav", 
+# Import only relevant variables for CFA
+s2rel <- read.spss(file = "S2_relevant_variables.sav", 
                         use.value.labels = FALSE, 
                         to.data.frame = TRUE)
