ec1e513893e513711da49591b55e26d48afec8b6
TonyRoberson <tonyjroberson@gmail.com>
2016-09-17 16:09:09 -0500

    Include Bollen-Stine, MLR, and WLSMV estimation for each of the 2, 3, and 4 factor models

--- Data/CFA Models.R
+++ Data/CFA Models.R
@@ -4,7 +4,7 @@
 library(lavaan)
 
 
-## TWO FACTOR MODEL ##
+## TWO FACTOR MODEL: Bollen-Stine ML ##
 
 
 # Specify the two factor model
@@ -26,7 +26,47 @@ summary(object = fit.2factor,
         standardized = TRUE)
 
 
-## THREE FACTOR MODEL ##
+## TWO FACTOR MODEL: MLR ##
+
+
+# Specify the two factor model
+Model.2factor <- '## Victimization factor
+                    Victimization =~ Q66A+Q66B+Q66C+Q66D+Q66E+Q66F+Q66G+Q66H+Q66I+Q66J+Q66K
+                  ## Perpetration factor
+                    Perpetration =~ Q68A+Q68B+Q68C+Q68D+Q68E+Q68F+Q68G+Q68H+Q68I+Q68J+Q68K
+                  ## Covariance between Vict and Perp
+                    Victimization ~~ Perpetration'
+# Fit the model as a CFA
+fit.2factor <- cfa(model = Model.2factor, 
+                   estimator = 'MLR',
+                   data = s2rel)
+# Print fit summary
+summary(object = fit.2factor,
+        fit.measures = TRUE,
+        standardized = TRUE)
+
+
+## TWO FACTOR MODEL ##
+
+
+# Specify the two factor model
+Model.2factor <- '## Victimization factor
+                    Victimization =~ Q66A+Q66B+Q66C+Q66D+Q66E+Q66F+Q66G+Q66H+Q66I+Q66J+Q66K
+                  ## Perpetration factor
+                    Perpetration =~ Q68A+Q68B+Q68C+Q68D+Q68E+Q68F+Q68G+Q68H+Q68I+Q68J+Q68K
+                  ## Covariance between Vict and Perp
+                    Victimization ~~ Perpetration'
+# Fit the model as a CFA
+fit.2factor <- cfa(model = Model.2factor, 
+                   estimator = 'WLSMV',
+                   data = s2rel)
+# Print fit summary
+summary(object = fit.2factor,
+        fit.measures = TRUE,
+        standardized = TRUE)
+
+
+## THREE FACTOR MODEL: Bollen-Stine ML ##
 
 
 # Specify the three factor model
@@ -50,7 +90,52 @@ summary(object = fit.3factor,
         standardized = TRUE)
 
 
-## FOUR FACTOR MODEL ##
+## THREE FACTOR MODEL: MLR ##
+
+
+# Specify the three factor model
+Model.3factor <-'## Latent factor specification
+                  tradVict =~ Q66A+Q66B+Q66C+Q66D+Q66E+Q66F+Q66G
+                  cyberVict  =~ Q66H+Q66I+Q66J+Q66K
+                  Perp =~ Q68A+Q68B+Q68C+Q68D+Q68E+Q68F+Q68G+Q68H+Q68I+Q68J+Q68K
+                 ## Latent variable covariances
+                  tradVict ~~ cyberVict
+                  tradVict ~~ Perp
+                  cyberVict ~~ Perp'
+# Fit the model as a CFA
+fit.3factor.MLR <- cfa(model = Model.3factor, 
+                   estimator = "MLR",
+                   data = s2rel)
+# Print fit summary
+summary(object = fit.3factor.MLR, 
+        fit.measures = TRUE,
+        standardized = TRUE)
+
+
+## THREE FACTOR MODEL: WLSMV ##
+
+
+# Specify the three factor model
+Model.3factor <-'## Latent factor specification
+tradVict =~ Q66A+Q66B+Q66C+Q66D+Q66E+Q66F+Q66G
+cyberVict  =~ Q66H+Q66I+Q66J+Q66K
+Perp =~ Q68A+Q68B+Q68C+Q68D+Q68E+Q68F+Q68G+Q68H+Q68I+Q68J+Q68K
+## Latent variable covariances
+tradVict ~~ cyberVict
+tradVict ~~ Perp
+cyberVict ~~ Perp'
+# Fit the model as a CFA
+fit.3factor.WLSMV <- cfa(model = Model.3factor, 
+                       estimator = "WLSMV",
+                       data = s2rel)
+# Print fit summary
+summary(object = fit.3factor.WLSMV, 
+        fit.measures = TRUE,
+        standardized = TRUE)
+
+
+
+## FOUR FACTOR MODEL: Bollen-Stine ML ##
 
 
 # Specify the four factor model
@@ -76,3 +161,56 @@ fit.4factor <- cfa(model = Model.4factor,
 summary(object = fit.4factor, 
         fit.measures = TRUE,
         standardized = TRUE)
+
+
+## FOUR FACTOR MODEL: MLR ##
+
+
+# Specify the four factor model
+Model.4factor <- '## Latent factor specification
+                    tradVict =~ Q66A+Q66B+Q66C+Q66D+Q66E+Q66F+Q66G
+                    cyberVict =~ Q66H+Q66I+Q66J+Q66K
+                    tradPerp =~ Q68A+Q68B+Q68C+Q68D+Q68E+Q68F+Q68G
+                    cyberPerp =~ Q68H+Q68I+Q68J+Q68K
+                  ## Latent variable covariances
+                    tradVict ~~ cyberVict
+                    tradVict ~~ tradPerp
+                    tradVict ~~ cyberPerp
+                    tradPerp ~~ cyberPerp
+                    tradPerp ~~ cyberVict
+                    cyberVict ~~ cyberPerp'
+# Fit the model as a CFA
+fit.4factor.MLR <- cfa(model = Model.4factor, 
+                   estimator = "MLR",
+                   data = s2rel)
+# Print fit summary
+summary(object = fit.4factor.MLR, 
+        fit.measures = TRUE,
+        standardized = TRUE)
+
+
+## FOUR FACTOR MODEL: WLSMV ##
+
+
+# Specify the four factor model
+Model.4factor <- '## Latent factor specification
+                    tradVict =~ Q66A+Q66B+Q66C+Q66D+Q66E+Q66F+Q66G
+                    cyberVict =~ Q66H+Q66I+Q66J+Q66K
+                    tradPerp =~ Q68A+Q68B+Q68C+Q68D+Q68E+Q68F+Q68G
+                    cyberPerp =~ Q68H+Q68I+Q68J+Q68K
+                  ## Latent variable covariances
+                    tradVict ~~ cyberVict
+                    tradVict ~~ tradPerp
+                    tradVict ~~ cyberPerp
+                    tradPerp ~~ cyberPerp
+                    tradPerp ~~ cyberVict
+                    cyberVict ~~ cyberPerp'
+# Fit the model as a CFA
+fit.4factor.WLSMV <- cfa(model = Model.4factor, 
+                       estimator = "WLSMV",
+                       data = s2rel)
+# Print fit summary
+summary(object = fit.4factor.WLSMV, 
+        fit.measures = TRUE,
+        standardized = TRUE)
+
