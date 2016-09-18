f91f7fb39dd2780a7f9d8f502f07fdb4ba754a94
TonyRoberson <tonyjroberson@gmail.com>
2016-09-18 13:17:30 -0500

    Change script to import only .csv files

--- Data/loadDataScript.R
+++ Data/loadDataScript.R
@@ -1,17 +1,9 @@
-# Load import package
-library(foreign)
+## Import datasets
 
 # Import first split half for EFA
-s1 <- read.spss(file = "HBSC Bullying Scale_S1_EFA.sav", 
-                     use.value.labels = FALSE, 
-                     to.data.frame = TRUE)
+S1 <- read.csv("HBSC Bullying Scale_S1_EFA.csv", 
+               stringsAsFactors=FALSE)
 
 # Import second split half for CFA
-s2 <- read.spss(file = "HBSC Bullying Scale_S2_CFA.sav", 
-          use.value.labels = FALSE,
-          to.data.frame = TRUE)
-
-# Import only relevant variables for CFA
-s2rel <- read.spss(file = "S2_relevant_variables.sav", 
-                        use.value.labels = FALSE, 
-                        to.data.frame = TRUE)
+S2 <- read.csv("HBSC Bullying Scale_S2_CFA.csv", 
+               stringsAsFactors=FALSE)
