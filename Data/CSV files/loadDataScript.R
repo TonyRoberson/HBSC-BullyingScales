19e3f8c2de33f6db6c8ad1b7824a2d6634acb7e7
TonyRoberson <tonyjroberson@gmail.com>
2016-09-18 13:51:36 -0500

    Deleted loadDataScript file as import calls added to EFA/CFA scripts

--- Data/CSV files/loadDataScript.R
+++
@@ -1,9 +0,0 @@
-## Import datasets
-
-# Import first split half for EFA
-S1 <- read.csv("HBSC Bullying Scale_S1_EFA.csv", 
-               stringsAsFactors=FALSE)
-
-# Import second split half for CFA
-S2 <- read.csv("HBSC Bullying Scale_S2_CFA.csv", 
-               stringsAsFactors=FALSE)
