8f474030615c9f2ee6bf2a6750ab23a02907fc79
TonyRoberson <tonyjroberson@gmail.com>
2016-09-18 13:39:59 -0500

    Move to CSV folder

---
+++ Data/CSV files/loadDataScript.R
@@ -0,0 +1,9 @@
+## Import datasets
+
+# Import first split half for EFA
+S1 <- read.csv("HBSC Bullying Scale_S1_EFA.csv", 
+               stringsAsFactors=FALSE)
+
+# Import second split half for CFA
+S2 <- read.csv("HBSC Bullying Scale_S2_CFA.csv", 
+               stringsAsFactors=FALSE)
